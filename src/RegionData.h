35d834aa1a43b4f0199843bd5d2009e8af2baa85
Hugo Boyer <hugo@makerbot.com>
2011-10-24 17:15:20 -0400

    Added a real RegionData class based on the slicer output document

---
+++ src/RegionData.h
@@ -0,0 +1,58 @@
+/**
+   MiracleGrue - Model Generator for toolpathing. <http://www.grue.makerbot.com>
+   Copyright (C) 2011 Far McKon <Far@makerbot.com>, Hugo Boyer (hugo@makerbot.com)
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU Affero General Public License as
+   published by the Free Software Foundation, either version 3 of the
+   License, or (at your option) any later version.
+
+*/
+
+#ifndef REGION_DATA_H_
+#define REGION_DATA_H_
+
+
+#include <vector>
+#include <fstream>
+#include "DataEnvelope.h"
+
+
+struct Material
+{
+	std::string name;
+	int extruder;
+	bool support;
+};
+
+struct Contour
+{
+	std::string materialName;
+	Polygon contour;
+	bool isHole; // true if the contour is a hole
+};
+
+//
+// The Region data is contains polygons
+// polygons for each extruders
+//
+class RegionData: public DataEnvelope {
+
+public:
+	double positionZ;
+	double layerThickness;
+
+
+	RegionData (double z, double thick);
+	virtual ~RegionData();
+
+	std::vector<Material> materials;
+	std::vector<Contour> contours;
+
+	void writeJsonContour(std::ostream &out) const;
+
+};
+
+
+
+#endif // REGION_DATA_H_
