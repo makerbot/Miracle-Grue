91a0bad3a26cc700d583df7c0dd47caded001e70
Hugo Boyer <hugo@makerbot.com>
2011-10-26 11:18:52 -0400

    Added missing RegionerTestCase.cc file

---
+++ src/unit_tests/RegionerTestCase.cc
@@ -0,0 +1,54 @@
+#include <fstream>
+
+#include <cppunit/config/SourcePrefix.h>
+#include "RegionerTestCase.h"
+
+#include "../Configuration.h"
+#include "../RegionerOperation.h"
+
+CPPUNIT_TEST_SUITE_REGISTRATION( RegionerTestCase );
+
+#define SINGLE_EXTRUDER_FILE_NAME "v29_single_xtruder_warmup.gcode"
+#define DUAL_EXTRUDER_FILE_NAME "v29_dual_xtruder_warmup.gcode"
+
+void configurePathTest(Configuration& config)
+{
+	config.gcodeFilename = SINGLE_EXTRUDER_FILE_NAME;
+}
+
+
+void RegionerTestCase::setUp()
+{
+
+}
+
+void RegionerTestCase::example()
+{
+
+	RegionerOperation operation;
+	SliceData inputData(0.2, 0.4);
+	RegionData outputData(0.2, 0.4);
+
+//  CPPUNIT_ASSERT_DOUBLES_EQUAL( 1.0, 1.1, 0.05 );
+//  CPPUNIT_ASSERT( 1 == 0 );
+  CPPUNIT_ASSERT( 1 == 1 );
+
+  long* l1 = new long(12);
+  long* l2 = new long(12);
+
+  CPPUNIT_ASSERT_EQUAL( 12, 12 );
+  CPPUNIT_ASSERT_EQUAL( 12L, 12L );
+  CPPUNIT_ASSERT_EQUAL( *l1, *l2 );
+
+  delete l1;
+  delete l2;
+
+  CPPUNIT_ASSERT( 12L == 12L );
+//  CPPUNIT_ASSERT_EQUAL( 12, 13 );
+//  CPPUNIT_ASSERT_DOUBLES_EQUAL( 12.0, 11.99, 0.5 );
+
+}
+
+
+
+
