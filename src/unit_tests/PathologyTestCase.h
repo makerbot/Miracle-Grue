c83ec2d592a40450993d52711488f096ff643add
Hugomatic <hugo@makerbot.com>
2012-05-18 18:20:52 -0400

    starting a new test

---
+++ src/unit_tests/PathologyTestCase.h
@@ -0,0 +1,29 @@
+#ifndef ROOFING_TEST_CASE_H_
+#define ROOFING_TEST_CASE_H_
+
+#include <cppunit/extensions/HelperMacros.h>
+
+
+
+class PathologyTestCase : public CPPUNIT_NS::TestFixture
+{
+
+private:
+
+	CPPUNIT_TEST_SUITE( RoofingTestCase );
+
+
+		CPPUNIT_TEST(testSimple);
+    CPPUNIT_TEST_SUITE_END();
+
+
+public:
+  void setUp();
+
+protected:
+
+  void testSimple();
+
+};
+
+#endif
