fe6555a01545177532f0edc8248c170559753604
Filipp Gelman <filipp@makerbot.com>
2012-06-13 17:25:31 -0400

    Unit tests for new Gantry class

---
+++ src/unit_tests/GantryTestCase.cc
@@ -0,0 +1,101 @@
+#include "UnitTestUtils.h"
+#include "GantryTestCase.h"
+
+#include "mgl/gcoder_gantry.h"
+#include "mgl/gcoder.h"
+
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+using namespace mgl;
+using namespace libthing;
+
+CPPUNIT_TEST_SUITE_REGISTRATION( GantryTestCase );
+
+void GantryTestCase::setUp(){
+	std::cout<< " Done" << endl;
+}
+
+void GantryTestCase::testInitConfig(){
+	GantryConfig gantryCfg;
+	Gantry gantry(gantryCfg);
+	
+	CPPUNIT_ASSERT_EQUAL(gantryCfg.get_start_x(), gantry.get_x());
+	CPPUNIT_ASSERT_EQUAL(gantryCfg.get_start_y(), gantry.get_y());
+	CPPUNIT_ASSERT_EQUAL(gantryCfg.get_start_z(), gantry.get_z());
+	CPPUNIT_ASSERT_EQUAL(gantryCfg.get_start_a(), gantry.get_a());
+	CPPUNIT_ASSERT_EQUAL(gantryCfg.get_start_b(), gantry.get_b());
+	CPPUNIT_ASSERT_EQUAL(gantryCfg.get_start_feed(), gantry.get_feed());
+}
+void GantryTestCase::testManualMove(){
+	static const Scalar x = 10, y = 20, z = 50;
+		
+	GantryConfig gantryCfg;
+	Gantry gantry(gantryCfg);
+	
+	gantry.set_x(x);
+	gantry.set_y(y);
+	gantry.set_z(z);
+	
+	CPPUNIT_ASSERT_EQUAL(x, gantry.get_x());
+	CPPUNIT_ASSERT_EQUAL(y, gantry.get_y());
+	CPPUNIT_ASSERT_EQUAL(z, gantry.get_z());
+}
+void GantryTestCase::testG1Move(){
+	static const Scalar x = 75, y = 42, z = 13;
+	
+	stringstream ss;
+	stringstream expected;
+	
+	GantryConfig gantryCfg;
+	Gantry gantry(gantryCfg);
+	
+	gantry.set_x(0);
+	gantry.set_y(0);
+	gantry.set_z(0);
+	gantry.set_feed(3200);
+	
+	expected << "G1 X" << x << " Y" << y << " Z" << z << 
+			" F" << gantry.get_feed() << " ()" << endl;
+	
+	try{
+		gantry.g1(ss, x, y, z, gantry.get_feed(), "");
+	} catch (mgl::Exception thrown){
+		std::cout << "g1 exception: \t" << thrown.error << endl;
+		CPPUNIT_ASSERT(false);
+	}
+	string estring = expected.str();
+	string astring = ss.str();
+	
+	cout << "Expected GCode: \t" << estring << endl;
+	cout << "Actual GCode:   \t" << astring << endl;
+	
+	CPPUNIT_ASSERT(estring == astring);
+}
+void GantryTestCase::testSquirtSnort(){
+	stringstream ss;
+	string s;
+	
+	GantryConfig gantryCfg;
+	Gantry gantry(gantryCfg);
+	
+	gantry.set_x(0);
+	gantry.set_y(0);
+	gantry.set_z(0);
+	gantry.set_feed(3200);
+	
+	
+	gantry.snort(ss, Vector2(), Extruder(), Extrusion());
+	s = ss.str();
+	cout << "Snort:  \t" << s << endl;
+	CPPUNIT_ASSERT(s.size() != 0);
+	ss.str("");
+	gantry.squirt(ss, Vector2(), Extruder(), Extrusion());
+	s = ss.str();
+	cout << "Squirt: \t" << s << endl;
+	CPPUNIT_ASSERT(s.size() != 0);
+	
+}
+
+
---
+++ src/unit_tests/GantryTestCase.h
@@ -0,0 +1,35 @@
+/* 
+ * File:   GantryTestCase.h
+ * Author: Dev
+ *
+ * Created on June 13, 2012, 3:58 PM
+ */
+
+#ifndef GANTRYTESTCASE_H
+#define	GANTRYTESTCASE_H
+
+#include <cppunit/extensions/HelperMacros.h>
+
+class GantryTestCase : public CPPUNIT_NS::TestFixture{
+	
+	CPPUNIT_TEST_SUITE( GantryTestCase );
+	
+	CPPUNIT_TEST( testInitConfig );
+	CPPUNIT_TEST( testManualMove );
+	CPPUNIT_TEST( testG1Move );
+	CPPUNIT_TEST( testSquirtSnort );
+	
+	CPPUNIT_TEST_SUITE_END();
+	
+public:
+	void setUp();
+protected:
+	void testInitConfig();
+	void testManualMove();
+	void testG1Move();
+	void testSquirtSnort();
+};
+
+
+#endif	/* GANTRYTESTCASE_H */
+
