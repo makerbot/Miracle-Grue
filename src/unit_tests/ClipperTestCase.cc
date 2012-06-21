91c3e7b60df83ed335a535c2de061d84e7ee3e81
Filipp Gelman <filipp@makerbot.com>
2012-06-21 13:51:25 -0400

    Modified ClipperTestCase to use abstractable path mechanism

--- src/unit_tests/ClipperTestCase.cc
+++ src/unit_tests/ClipperTestCase.cc
@@ -38,6 +38,10 @@ public:
 void ClipperTestCase::setUp()
 {
 	MyComputer computer;
+	char pathsep = computer.fileSystem.getPathSeparatorCharacter();
+	outputDir = string("outputs") + pathsep + string("test_cases") + 
+			string("ClipperTestCase") + pathsep;
+	computer.fileSystem.guarenteeDirectoryExistsRecursive(outputDir.c_str());
 	mkDebugPath(outputDir.c_str());
 }
 
