2104086ebecde0727491494e298ab3dcba5d73ab
Joe Sadusk <joseph.sadusk@makerbot.com>
2012-06-26 18:31:21 -0400

    Implemented many tests, doesn't work yet

--- src/unit_tests/LoopPathTestCase.cc
+++ src/unit_tests/LoopPathTestCase.cc
@@ -13,7 +13,7 @@ using namespace libthing;
 CPPUNIT_TEST_SUITE_REGISTRATION( LoopPathTestCase );
 
 void LoopPathTestCase::setUp() {
-	cout << "No setup" << endl;
+	cout << endl << "No setup" << endl;
 }
 
 void LoopPathTestCase::testOpenPathBasic() {
@@ -27,12 +27,18 @@ void LoopPathTestCase::testOpenPathBasic() {
 	CPPUNIT_ASSERT_EQUAL(first.x, 5.0);
 	CPPUNIT_ASSERT_EQUAL(first.y, 5.0);
 
+	++i;
+	CPPUNIT_ASSERT(i == path.end());
+
 	path.appendPoint(Vector2(5, 6));
 	i = path.fromStart();
 	++i;
 	Vector2 second = *i;
 	CPPUNIT_ASSERT_EQUAL(second.x, 5.0);
 	CPPUNIT_ASSERT_EQUAL(second.y, 6.0);
+
+	++i;
+	CPPUNIT_ASSERT(i == path.end());
 
 	path.prependPoint(Vector2(6, 5));
 	i = path.fromStart();
@@ -43,7 +49,7 @@ void LoopPathTestCase::testOpenPathBasic() {
 	LineSegment2 seg = path.segmentAfterPoint(i);
 	CPPUNIT_ASSERT_EQUAL(seg.length(), 1.0);
 
-	OpenPath::iterator entry = path.getEntryPoints();
+	OpenPath::entry_iterator entry = path.getEntryPoints();
 	Vector2 first_entry = *entry;
 	Vector2 start = *(path.fromStart());
 	CPPUNIT_ASSERT_EQUAL(first_entry.x, start.x);
@@ -61,15 +67,99 @@ void LoopPathTestCase::testOpenPathBasic() {
 
 	Vector2 second_exit = path.getExitPoint(second_entry);
 	CPPUNIT_ASSERT_EQUAL(second_exit.x, first_entry.x);
-	CPPUNIT_ASSERT_EQUAL(second_exit.y, second_entry.y);
+	CPPUNIT_ASSERT_EQUAL(second_exit.y, first_entry.y);
 }
 
 void LoopPathTestCase::testLoopBasic() {
+	cout << "Testing single point loop" << endl;
 	
+	Loop loop(Vector2(2, 3));
+
+	Loop::cw_iterator i = loop.clockwise();
+	Vector2 first = *i;
+	CPPUNIT_ASSERT_EQUAL(first.x, 2.0);
+	CPPUNIT_ASSERT_EQUAL(first.y, 3.0);
+
+	++i;
+	Vector2 second = *i;
+	CPPUNIT_ASSERT_EQUAL(second.x, 2.0);
+	CPPUNIT_ASSERT_EQUAL(second.y, 3.0);
+
+	cout << "Testing two point loop" << endl;
+	i = loop.insertPoint(Vector2(4, 5), i);
+	first = *i;
+	CPPUNIT_ASSERT_EQUAL(first.x, 4.0);
+	CPPUNIT_ASSERT_EQUAL(first.y, 5.0);
+
+	++i;
+	second = *i;
+	CPPUNIT_ASSERT_EQUAL(second.x, 2.0);
+	CPPUNIT_ASSERT_EQUAL(second.y, 3.0);
+
+	++i;
+	first = *i;
+	CPPUNIT_ASSERT_EQUAL(first.x, 4.0);
+	CPPUNIT_ASSERT_EQUAL(first.y, 5.0);
+
+	cout << "Testing three point loop" << endl;
+	i = loop.insertPoint(Vector2(6, 7), i);
+	Loop::ccw_iterator j = loop.counterClockwise(*i);
+	first = *i;
+	CPPUNIT_ASSERT_EQUAL(first.x, 6.0);
+	CPPUNIT_ASSERT_EQUAL(first.y, 7.0);
+	
+	++i;
+	second = *i;
+	CPPUNIT_ASSERT_EQUAL(second.x, 4.0);
+	CPPUNIT_ASSERT_EQUAL(second.y, 5.0);
+
+	++i;
+	Vector2 third = *i;
+	CPPUNIT_ASSERT_EQUAL(third.x, 2.0);
+	CPPUNIT_ASSERT_EQUAL(third.y, 3.0);
+
+	++i;
+	Vector2 fourth = *i;
+	CPPUNIT_ASSERT_EQUAL(fourth.x, 6.0);
+	CPPUNIT_ASSERT_EQUAL(fourth.y, 7.0);
+	
+	cout << "Testing segmentAfterPoint" << endl;
+	LineSegment2 seg = loop.segmentAfterPoint(i);
+	CPPUNIT_ASSERT_EQUAL(seg.length(), 1.0);
+
+	cout << "Testing entry points" << endl;
+	int count = 1;
+	Loop::entry_iterator entry = loop.getEntryPoints();
+	for (;	entry !+ loop.entryEnd(); entry++)
+		count++;
+
+	CPPUNIT_ASSERT_EQUAL(count, 3);
+
+	Vector2 exitpoint = loop.getExitPoint(entry);
+	Vector2 entrypoint = *entry;
+	CPPUNIT_ASSERT_EQUAL(entrypoint.x, exitpoint.x);
+	CPPUNIT_ASSERT_EQUAL(entrypoint.y, exitpoint.y);
 }
 
 void LoopPathTestCase::testLoopPathBasic() {
-	
+	Loop loop(Vector(1, 2));
+
+	Loop::cw_iterator last = loop.insertPoint(Vector(3, 4), loop.clockwise());
+
+	LoopPath lp(loop.clockwise(Vector(1, 2)), Vector(1, 2));
+
+	LoopPath::iterator i = lp.fromStart();
+	Vector2 first = *i;
+	CPPUNIT_ASSERT_EQUAL(first.x, 1.0);
+	CPPUNIT_ASSERT_EQUAL(first.y, 2.0);
+
+	++i;
+	Vector2 second = *i;
+	CPPUNIT_ASSERT_EQUAL(second.x, 3.0);
+	CPPUNIT_ASSERT_EQUAL(second.y, 4.0);
+
+	++i;
+	CPPUNIT_ASSERT(i == lp.end());
 }
 
 void LoopPathTestCase::testOpenPathJoin() {
