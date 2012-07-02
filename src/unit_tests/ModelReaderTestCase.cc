f4b432dc3e2df8f9b73bf564edd39a99a533b7f4
Filipp Gelman <filipp@makerbot.com>
2012-07-02 11:45:41 -0400

    Removed references to connexity. Now it is not necessary for building

--- src/unit_tests/ModelReaderTestCase.cc
+++ src/unit_tests/ModelReaderTestCase.cc
@@ -18,7 +18,6 @@
 
 #include "UnitTestUtils.h"
 
-#include "mgl/connexity.h"
 #include "mgl/configuration.h"
 #include "mgl/slicy.h"
 #include "mgl/segmenter.h"
@@ -59,41 +58,41 @@ void ModelReaderTestCase::setUp()
 // Adds 2 triangles with a common edge
 // to a Slicy
 //
-void ModelReaderTestCase::testSlicySimple()
-{
-
-	Vector3 p0(0,0,0);
-	Vector3 p1(0,1,0);
-	Vector3 p2(1,1,0);
-	Vector3 p3(1,0,0);
-
-	Triangle3 t0(p0, p1, p2);
-	Triangle3 t1(p0, p2, p3);
-
-	Connexity sy(0.001);
-
-	cout << endl << endl;
-	cout << "******** slicy with 2 triangles *******" << endl;
-	cout << "add t0" << endl;
-	size_t face0 = sy.addTriangle(t0);
-
-	cout << "add t1" << endl;
-	size_t face1 = sy.addTriangle(t1);
-
-	cout << "xx"<< endl;
-
-
-
-	int a,b,c;
-	sy.lookupIncidentFacesToFace(face0, a,b,c);
-
-	cout << "xx"<< endl;
-	CPPUNIT_ASSERT(a==face1 || b==face1 || c== face1);
-
-	cout << "xxx"<< endl;
-	sy.dump(cout);
-//	cout << sy << endl;
-}
+//void ModelReaderTestCase::testSlicySimple()
+//{
+//
+//	Vector3 p0(0,0,0);
+//	Vector3 p1(0,1,0);
+//	Vector3 p2(1,1,0);
+//	Vector3 p3(1,0,0);
+//
+//	Triangle3 t0(p0, p1, p2);
+//	Triangle3 t1(p0, p2, p3);
+//
+//	Connexity sy(0.001);
+//
+//	cout << endl << endl;
+//	cout << "******** slicy with 2 triangles *******" << endl;
+//	cout << "add t0" << endl;
+//	size_t face0 = sy.addTriangle(t0);
+//
+//	cout << "add t1" << endl;
+//	size_t face1 = sy.addTriangle(t1);
+//
+//	cout << "xx"<< endl;
+//
+//
+//
+//	int a,b,c;
+//	sy.lookupIncidentFacesToFace(face0, a,b,c);
+//
+//	cout << "xx"<< endl;
+//	CPPUNIT_ASSERT(a==face1 || b==face1 || c== face1);
+//
+//	cout << "xxx"<< endl;
+//	sy.dump(cout);
+////	cout << sy << endl;
+//}
 
 //
 // This test loads 1 triangle and verifies the number of slices
@@ -329,42 +328,42 @@ void ModelReaderTestCase::testMeshyCycleMin()
 }
 
 
-void ModelReaderTestCase::testSlicyWater()
-{
-	Meshy mesh;
-	Segmenter seg(0.35, 0.35);
-	string target = inputsDir + "Water.stl";
-	mesh.readStlFile( target.c_str());
-	seg.tablaturize(mesh);
-	const SliceTable& table = seg.readSliceTable();
-
-	unsigned int t0,t1;
-	t0=clock();
-
-	const vector<Triangle3>& allTriangles = mesh.readAllTriangles();
-	int sliceIndex = 0;
-	for (SliceTable::const_iterator i = table.begin(); i != table.end(); i++)
-	{
-		const TriangleIndices& sliceables = *i;
-		t1=clock()-t0;
-		cout << "clock: " << t1 << endl;
-		cout << "Slice: " << sliceIndex  << " ("<< sliceables.size()  << " triangles)" << endl;
-
-		Connexity sy(0.000000001);
-		cout  << " Triangles in this slice" << endl;
-		for (TriangleIndices::const_iterator j = sliceables.begin(); j != sliceables.end(); j++ )
-		{
-			index_t index = (*j);
-			const Triangle3& triangle = allTriangles[index];
-//			cout << "adding triangle # " << index << endl;
-			sy.addTriangle(triangle);
-		}
-//		cout << sy << endl;
-		sliceIndex ++;
-	}
-	t1=clock()-t0;
-	cout << "clock: " << t1 << endl;
-}
+//void ModelReaderTestCase::testSlicyWater()
+//{
+//	Meshy mesh;
+//	Segmenter seg(0.35, 0.35);
+//	string target = inputsDir + "Water.stl";
+//	mesh.readStlFile( target.c_str());
+//	seg.tablaturize(mesh);
+//	const SliceTable& table = seg.readSliceTable();
+//
+//	unsigned int t0,t1;
+//	t0=clock();
+//
+//	const vector<Triangle3>& allTriangles = mesh.readAllTriangles();
+//	int sliceIndex = 0;
+//	for (SliceTable::const_iterator i = table.begin(); i != table.end(); i++)
+//	{
+//		const TriangleIndices& sliceables = *i;
+//		t1=clock()-t0;
+//		cout << "clock: " << t1 << endl;
+//		cout << "Slice: " << sliceIndex  << " ("<< sliceables.size()  << " triangles)" << endl;
+//
+//		Connexity sy(0.000000001);
+//		cout  << " Triangles in this slice" << endl;
+//		for (TriangleIndices::const_iterator j = sliceables.begin(); j != sliceables.end(); j++ )
+//		{
+//			index_t index = (*j);
+//			const Triangle3& triangle = allTriangles[index];
+////			cout << "adding triangle # " << index << endl;
+//			sy.addTriangle(triangle);
+//		}
+////		cout << sy << endl;
+//		sliceIndex ++;
+//	}
+//	t1=clock()-t0;
+//	cout << "clock: " << t1 << endl;
+//}
 
 
 
@@ -828,75 +827,75 @@ void initConfig(Configuration &config)
 	config["slicer"]["layerH"] = 0.35;
 }
 
-void slicyTest()
-{
-	cout << endl;
-	string modelFile = "inputs/3D_Knot.stl";
-
-    Configuration config;
-    initConfig(config);
-	Meshy mesh;
-	Segmenter seg(config["slicer"]["firstLayerZ"].asDouble(), config["slicer"]["layerH"].asDouble()); // 0.35
-	mesh.readStlFile(modelFile.c_str());
-	seg.tablaturize(mesh);
-	cout << "file " << modelFile << endl;
-	const SliceTable &sliceTable = seg.readSliceTable();
-	int layerCount = sliceTable.size();
-	cout  << "Slice count: "<< layerCount << endl;
-	const vector<Triangle3> &allTriangles = mesh.readAllTriangles();
-	cout << "Faces: " << allTriangles.size() << endl;
-	cout << "layer " << layerCount-1 << " z: " << seg.readLayerMeasure().sliceIndexToHeight(layerCount-1) << endl;
-
-	int layerIndex = 44;
-	CPPUNIT_ASSERT (layerIndex < layerCount);
-	const TriangleIndices &trianglesInSlice = sliceTable[layerIndex];
-	unsigned int triangleCount = trianglesInSlice.size();
-	Scalar z = seg.readLayerMeasure().sliceIndexToHeight(layerIndex);
-	cout << triangleCount <<" triangles in layer " << layerIndex  << " z = " << z << endl;
-
-	// Load slice connectivity information
-	Connexity connexity(1e-6);
-	for (int i=0; i < triangleCount; i++)
-	{
-		unsigned int triangleIndex = trianglesInSlice[i];
-		const Triangle3& t = allTriangles[triangleIndex];
-		connexity.addTriangle(t);
-	}
-
-	cout << connexity << endl;
-
-	list<index_t> faces;
-	size_t faceCount = connexity.readFaces().size();
-	for(index_t i=0; i< faceCount; i++)
-	{
-		faces.push_back(i);
-
-	}
-
-
-	const Face &face = connexity.readFaces()[0];
-	LineSegment2 cut;
-	bool success = connexity.cutFace(z, face, cut);
-	cout << "FACE cut " << cut.a << " to " << cut.b << endl;
-	CPPUNIT_ASSERT(success);
-
-	list<LineSegment2> loop;
-	connexity.splitLoop(z, faces, loop);
-
-	cout << "First loop has " << loop.size() << " segments" << endl;
-
-	size_t i=0;
-	for(list<LineSegment2>::iterator it = loop.begin(); it != loop.end(); it++)
-	{
-		cout << i << "] " << it->a << ", " << it->b << endl;
-		i++;
-	}
-
-	cout << "loop with " << loop.size() << "faces" << endl;
-	cout << "faces left: "  << faces.size()  << " / " << connexity.readEdges().size() << endl;
-
-//	list<index_t> edges;
-//	slicy.fillEdgeList(z,edges);
-//	dumpIntList(edges);
-
-}
+//void slicyTest()
+//{
+//	cout << endl;
+//	string modelFile = "inputs/3D_Knot.stl";
+//
+//    Configuration config;
+//    initConfig(config);
+//	Meshy mesh;
+//	Segmenter seg(config["slicer"]["firstLayerZ"].asDouble(), config["slicer"]["layerH"].asDouble()); // 0.35
+//	mesh.readStlFile(modelFile.c_str());
+//	seg.tablaturize(mesh);
+//	cout << "file " << modelFile << endl;
+//	const SliceTable &sliceTable = seg.readSliceTable();
+//	int layerCount = sliceTable.size();
+//	cout  << "Slice count: "<< layerCount << endl;
+//	const vector<Triangle3> &allTriangles = mesh.readAllTriangles();
+//	cout << "Faces: " << allTriangles.size() << endl;
+//	cout << "layer " << layerCount-1 << " z: " << seg.readLayerMeasure().sliceIndexToHeight(layerCount-1) << endl;
+//
+//	int layerIndex = 44;
+//	CPPUNIT_ASSERT (layerIndex < layerCount);
+//	const TriangleIndices &trianglesInSlice = sliceTable[layerIndex];
+//	unsigned int triangleCount = trianglesInSlice.size();
+//	Scalar z = seg.readLayerMeasure().sliceIndexToHeight(layerIndex);
+//	cout << triangleCount <<" triangles in layer " << layerIndex  << " z = " << z << endl;
+//
+//	// Load slice connectivity information
+//	Connexity connexity(1e-6);
+//	for (int i=0; i < triangleCount; i++)
+//	{
+//		unsigned int triangleIndex = trianglesInSlice[i];
+//		const Triangle3& t = allTriangles[triangleIndex];
+//		connexity.addTriangle(t);
+//	}
+//
+//	cout << connexity << endl;
+//
+//	list<index_t> faces;
+//	size_t faceCount = connexity.readFaces().size();
+//	for(index_t i=0; i< faceCount; i++)
+//	{
+//		faces.push_back(i);
+//
+//	}
+//
+//
+//	const Face &face = connexity.readFaces()[0];
+//	LineSegment2 cut;
+//	bool success = connexity.cutFace(z, face, cut);
+//	cout << "FACE cut " << cut.a << " to " << cut.b << endl;
+//	CPPUNIT_ASSERT(success);
+//
+//	list<LineSegment2> loop;
+//	connexity.splitLoop(z, faces, loop);
+//
+//	cout << "First loop has " << loop.size() << " segments" << endl;
+//
+//	size_t i=0;
+//	for(list<LineSegment2>::iterator it = loop.begin(); it != loop.end(); it++)
+//	{
+//		cout << i << "] " << it->a << ", " << it->b << endl;
+//		i++;
+//	}
+//
+//	cout << "loop with " << loop.size() << "faces" << endl;
+//	cout << "faces left: "  << faces.size()  << " / " << connexity.readEdges().size() << endl;
+//
+////	list<index_t> edges;
+////	slicy.fillEdgeList(z,edges);
+////	dumpIntList(edges);
+//
+//}
