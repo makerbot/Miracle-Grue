c169620333ca6027a76c537d2d3894234c85fc76
Filipp Gelman <filipp@makerbot.com>
2012-07-09 11:40:12 -0400

    Modified loop_path to not use static_cast for backwards compatibility

--- src/mgl/loop_path.h
+++ src/mgl/loop_path.h
@@ -571,7 +571,7 @@ public:
 	cw_iterator clockwise(const PointType &startpoint) {
 		for (PointNormalList::iterator i = pointNormals.begin();
 			 i != pointNormals.end(); i++) {
-			if (static_cast<PointType>(*i) == startpoint)
+			if (i->getPoint() == startpoint)
 				return cw_iterator(i, pointNormals.begin(), 
 						pointNormals.end());
 		}
@@ -580,7 +580,7 @@ public:
 	const_cw_iterator clockwise(const PointType &startpoint) const {
 		for (PointNormalList::const_iterator i = pointNormals.begin();
 			 i != pointNormals.end(); i++) {
-			if (static_cast<PointType>(*i) == startpoint)
+			if (i->getPoint() == startpoint)
 				return const_cw_iterator(i, pointNormals.begin(), 
 						pointNormals.end());
 		}
@@ -627,7 +627,7 @@ public:
 	ccw_iterator counterClockwise(const PointType &startpoint) {
 		for (PointNormalList::reverse_iterator i = pointNormals.rbegin();
 			 i != pointNormals.rend(); i++) {
-			if (static_cast<PointType>(*i) == startpoint)
+			if (i->getPoint() == startpoint)
 				return ccw_iterator(i, pointNormals.rbegin(), 
 						pointNormals.rend());
 		}
@@ -636,7 +636,7 @@ public:
 	const_ccw_iterator counterClockwise(const PointType& startpoint) const {
 		for (PointNormalList::const_reverse_iterator i = pointNormals.rbegin();
 			 i != pointNormals.rend(); i++) {
-			if (static_cast<PointType>(*i) == startpoint)
+			if (i->getPoint() == startpoint)
 				return const_ccw_iterator(i, pointNormals.rbegin(), 
 						pointNormals.rend());
 		}
