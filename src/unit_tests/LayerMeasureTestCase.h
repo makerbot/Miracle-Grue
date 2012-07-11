293e227543b7b735173cc99645acc7a0ea4cb2b9
Filipp Gelman <filipp@makerbot.com>
2012-07-11 11:14:03 -0400

    Implemented basic unit tests for LayerMeasure

--- src/unit_tests/LayerMeasureTestCase.h
+++ src/unit_tests/LayerMeasureTestCase.h
@@ -7,12 +7,17 @@
 class LayerMeasureTestCase : public CPPUNIT_NS::TestFixture {
 	
 	CPPUNIT_TEST_SUITE( LayerMeasureTestCase );
+	CPPUNIT_TEST( testLayer0 );
+	CPPUNIT_TEST( testCreatingLayers );
+	CPPUNIT_TEST( testOffset );
 	CPPUNIT_TEST_SUITE_END();
 	
 public:
 	void setUp();
 protected:
-	
+	void testLayer0();
+	void testCreatingLayers();
+	void testOffset();
 };
 
 
