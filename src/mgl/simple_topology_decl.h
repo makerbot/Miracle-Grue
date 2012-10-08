36e48e1ede5c48fdb418d7a29a377c7437b81d62
Filipp Gelman <filipp@makerbot.com>
2012-10-08 11:00:43 -0400

    Start of a simplified graph object

---
+++ src/mgl/simple_topology_decl.h
@@ -0,0 +1,73 @@
+#ifndef MGL_SIMPLE_TOPOLOGY_DECL_H
+#define	MGL_SIMPLE_TOPOLOGY_DECL_H
+
+#include <utility>
+#include <memory>
+#include <vector>
+#include <map>
+
+namespace topo {
+
+template <typename _NODE_DATA_T, typename _COST_T>
+class simple_graph {
+public:
+    
+    class node;
+    class node_info_group;
+    
+    typedef _NODE_DATA_T node_data_type;
+    typedef _COST_T cost_type;
+    
+    typedef std::map<size_t, cost_type> adjacency_map;
+    typedef std::map<size_t, size_t> reverse_adjacency_map;
+    
+    typedef std::vector<node_info_group> node_container_type;
+    
+    class node {
+    public:
+        friend class simple_graph;
+        friend class node_info_group;
+        friend class node_container_type::allocator_type;
+        void connect(const node& other, const cost_type& cost);
+        void disconnect(const node& other);
+    private:
+        node(simple_graph& parent, size_t index, 
+                const node_data_type& data = node_data_type());
+        node(const node& other);                //allocators only
+        node& operator =(const node& other);    //NOT ALLOWED
+        
+        inline size_t getIndex() const { return m_index; }
+        
+        simple_graph& m_parent;
+        const size_t m_index;
+        const node_data_type m_data;
+    };
+    
+    template <typename COST_GEN>
+    void connect(node& a, node& b, const COST_GEN& costGenerator = COST_GEN());
+    void connect(node& a, node& b, const cost_type& cost);
+    void disconnect(node& a, node& b);
+    
+    node& createNode(const node_data_type& data = node_data_type());
+    
+private:
+    
+    class node_info_group {
+    public:
+        node_info_group(simple_graph& parent, size_t index, 
+                const node_data_type& data = node_data_type());
+        node_info_group(const node& other);
+        node_info_group& operator =(const node_info_group& other); // NOT ALLOWED
+        
+        node m_node;
+        adjacency_map m_forward_links;
+        reverse_adjacency_map m_reverse_links;
+    };
+    
+    node_container_type graph;
+};
+
+}
+
+#endif	/* MGL_SIMPLE_TOPOLOGY_DECL_H */
+
