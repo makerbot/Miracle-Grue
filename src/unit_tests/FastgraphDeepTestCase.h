66e15ca488d50c6d4e00a03659d033e823459ed5
Filipp Gelman <filipp@makerbot.com>
2012-11-02 08:20:56 -0400

    Unit test for hierarchical loop ordering

---
+++ src/unit_tests/FastgraphDeepTestCase.h
@@ -0,0 +1,21 @@
+#ifndef MGL_UNIT_TEST_FASTGRAPHDEEPTESTCASE_H_
+#define	MGL_UNIT_TEST_FASTGRAPHDEEPTESTCASE_H_
+
+#include <cppunit/extensions/HelperMacros.h>
+#define FASTGRAPH_FRIENDS_LIST friend class FastgraphDeepTestCase;
+#include "mgl/pather_optimizer_fastgraph.h"
+
+class FastgraphDeepTestCase : public CPPUNIT_NS::TestFixture {
+    CPPUNIT_TEST_SUITE ( FastgraphDeepTestCase );
+    CPPUNIT_TEST( testLoopOrdering );
+    CPPUNIT_TEST_SUITE_END();
+public:
+    void setUp() {}
+protected:
+    void testLoopOrdering();
+private:
+    void displayBucket(mgl::pather_optimizer_fastgraph::bucket& bucket);
+};
+
+#endif	/* MGL_UNIT_TEST_FASTGRAPHDEEPTESTCASE_H_ */
+
