e0a212c453894eb56cdfb88a3f6fedc83a2b150c
Filipp Gelman <filipp@makerbot.com>
2012-11-30 09:05:50 -0500

    M127 turns fan off, M126 turns it on

--- src/mgl/gcoder.cc
+++ src/mgl/gcoder.cc
@@ -364,7 +364,7 @@ void GCoder::writeSlice(std::ostream& ss,
         if (grueCfg.get_weightedFanCommand() != -1)
             ss << "M106 S" << grueCfg.get_weightedFanCommand();
         else 
-            ss << "M127 T" << grueCfg.get_defaultExtruder();
+            ss << "M126 T" << grueCfg.get_defaultExtruder();
         
         ss << " " << grueCfg.get_commentOpen()
            << "Turn on the fan"
